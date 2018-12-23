b304db3a227f46528fe3a998f4a7777285f0c0e0
Oliver 2wist <joliver017@gmail.com>
2018-12-22 20:32:26 -0600

    got numberOfGuesses to only decrease if wrong letter pressed, but it seems to ignore the first guess (essentially giving a freebie); created code to reset the game upon win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,35 @@
-var wordBank = ["squirtle","bulbasaur", "charmander","pikachu","gengar","alakazam","pidgeot","mewtwo","snorlax","arcanine"]
+var wordBank = ["squirtle","bulbasaur", "charmander","pikachu","gengar","alakazam","pidgeot","mewtwo","snorlax","arcanine"];
 var lettersGuessed = [];
 var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 var wordBlanks = [];
 
 var numberOfGuesses = 10;
-var wins = 0;
+var wins = 1;
 var letter = "";
 
 
 document.getElementById("guessesRemaining").innerHTML = "Number of guesses Remaining:" + " " + numberOfGuesses;
-document.getElementById("chosenword").innerHTML = "";
-for (var i = 0; i < wordBlanks.length; i++) {
-    document.getElementById("chosenword").innerHTML += wordBlanks[i];
-}
 
-for(var i = 0; i < randWord.length; i++) {
+// *****I don't think I need this code anymore
+
+// document.getElementById("chosenword").innerHTML = "";
+// for (var i = 0; i < wordBlanks.length; i++) {
+   // document.getElementById("chosenword").innerHTML += wordBlanks[i];
+// }
+
+function startGame() {
+    numberOfGuesses = 10;
+    lettersGuessed = [];
+    wordBlanks = [];
+    randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    for(var i = 0; i < randWord.length; i++) {
     wordBlanks.push('_');
     document.getElementById("chosenword").innerHTML = wordBlanks.join(' ');
 }
+};
+
+startGame();
 
 console.log(wordBlanks);
 console.log(randWord); // Check which word was chosen from the wordBank array
@@ -26,9 +38,9 @@ document.onkeydown = function(event) {
     if(event.keyCode >= 65 && event.keyCode <= 90) {
         letter = event.key;
         event.key.toLowerCase();
-        
+        makeGuess(letter); 
         document.getElementById("lettersGuessed").innerHTML = "Letters already guessed: " + lettersGuessed;
-        makeGuess(letter);   
+        youWon();
         console.log(event);
         console.log(lettersGuessed);   
     }
@@ -53,7 +65,26 @@ function checkGuess(letter) {
 
     if(wordBlanks.indexOf(letter) === -1) {
         document.getElementById("guessesRemaining").innerHTML = "Number of guesses Remaining:" + " " + numberOfGuesses--;
-        };
+        if(numberOfGuesses === -1) {
+            alert("you lose");
+            startGame();
+            console.log(wordBlanks);
+            console.log(randWord);
+    };    
+    };
+    
+};
+
+
+
+function youWon() {
+    if(wordBlanks.indexOf("_") === -1) {
+        document.getElementById("totalWins").innerHTML = "Wins: " + wins++;
+        alert("you win");
+        startGame();
+        console.log(wordBlanks);
+        console.log(randWord);
+    };
 };
 
 
